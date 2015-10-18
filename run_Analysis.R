dd47892ee0890e2b4c1a49a6c929f49e14efe33a
ds598p <ds598p@users.noreply.github.com>
2015-10-18 10:27:09 -0400

    Update run_Analysis.R
    Cleaned up commentary to match readme.md

--- run_Analysis.R
+++ run_Analysis.R
@@ -1,6 +1,6 @@
 ## Project Getting & Cleaning Data
 
-## verify needed libraries
+##  Phase 1: verify needed libraries
 
 install.packages("data.table")
 library(data.table)
@@ -11,17 +11,17 @@ library(sqldf)
 install.packages("reshape2")
 library(reshape2)
 
-## verify directory
+## Phase 2: verify directory
 
 if(!file.exists("GDCproj")){dir.create("GDCproj")}
 
-## Download project data
+## Phase 3: Download project data
 
 pdURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(pdURL, dest="./GDCproj/pdata.zip")
 unzip ("./GDCproj/pdata.zip")
 
-## read data
+## Phase 4: read data
 
 testSet <- read.table("./UCI HAR Dataset/test/x_test.txt")
 trainSet <- read.table("./UCI HAR Dataset/train/x_train.txt")
@@ -35,33 +35,33 @@ trainActivity <- read.table("./UCI HAR Dataset/train/y_train.txt")
 headers <- read.table("./UCI HAR Dataset/features.txt")
 activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
-## applying activity labels
+## Phase 5:  applying activity labels
 
 testActivityLabels <- sqldf("select a.*, b.v2 from testActivity a join activityLabels b on a.v1 = b.v1")
 trainActivityLabels <- sqldf("select a.*, b.v2 from trainActivity a join activityLabels b on a.v1 = b.v1")
 
-## merging data sets
+## Phase 6:  merging data sets
 
 testSetFull <-cbind(testSubject,testActivityLabels,testSet)
 trainSetFull <-cbind(trainSubject,trainActivityLabels,trainSet)
 
-##  combine data sets then apply headers
+##  Phase 7:  combine data sets then apply headers
 
 setFull <- rbind(testSetFull,trainSetFull)
 headNames <- as.character(headers[,2])
 colnames(setFull) <- c("Subject","Act","Activity",headNames)
 
-## subsetting final table to only desired columns
+## Phase 8:  subsetting final table to only desired columns
 
 meanStdVars <- sqldf("select v2 from headers where v2 like '%mean(%' or v2 like '%std(%'")
 colSelect <- c("Subject","Activity",as.character(meanStdVars[,1]))
 setUse <- subset(setFull,,colSelect)
 
-## melting and casting to get means of attributes
+## Phase 9:  melting and casting to get means of attributes
 
 meltSet <- melt(setUse,id = c("Subject","Activity"),measure.vars=c(as.character(meanStdVars[,1])))
 castSet <- dcast(meltSet,Subject+Activity~variable,mean)
 
-## output tidy text file
+## Phase 10:  output tidy text file
 
 write.table(castSet,file = "Activity_Means_Tidy.txt", row.name=F)
